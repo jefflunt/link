93ac197055bae8c424791171b30aafe87808876b
jefflunt <jefflunt@gmail.com>
2025-02-21 17:12:52 -0600

    start on app routing; do some tests to ensure we can create url records; test auto-generation of short code

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,10 @@
+class UrlsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def redirect
+  end
+end
---
+++ app/models/url.rb
@@ -0,0 +1,17 @@
+class Url < ApplicationRecord
+  validates :dest,
+            presence: true
+
+  validates :dest,
+            length: {
+              minimum: 10,    # 'http://a.a'
+              maximum: 2048
+            }
+
+  before_create :_generate_code
+
+  def _generate_code
+    characters = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+    self.code = (0...8).map { characters.sample }.join
+  end
+end
